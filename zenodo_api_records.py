3ad707cb4701c316d374a8287ff8d7f0090788f5
Davide Cirillo <cirillod@gmail.com>
2022-03-22 20:24:40 +0100

    Add files via upload

--- zenodo_api_records.py
+++ zenodo_api_records.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 import numpy as np
 
-ACCESS_TOKEN = <your access token>
+ACCESS_TOKEN = 'vf2H4OcPRXU7PzKKIFgkN3BUeoiGkrQEx6cg7lLqQUbECPUfEYwBrZos9X2y'
 
 # query request
 response = requests.get('https://zenodo.org/api/records',
@@ -23,9 +23,9 @@ for hit in dict_data['hits']['hits']:
 	title = hit['metadata']['title']
 	description = hit['metadata']['description']
 
-	lst.append([doi, access_right, title, description])
+	lst.append([doi, access_right, title])#, description])
 df = pd.DataFrame.from_records(lst)
 
 # save to file
-df.columns = ['doi','access_right','title','description']
+df.columns = ['doi','access_right','title']#,'description']
 df.to_csv('zenodo_open_synth_datasets.csv',index=None)
